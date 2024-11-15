d29e353806db412df6088c938b6848fa1cc2feaa
haohiao <haohiao@qq.com>
2024-11-15 11:44:01 +0800

    add xaml: Basic XAML and Events In XAML

---
+++ xaml/EventsInXAML/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventsInXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show($"({e.GetPosition(sender as Grid)})");
+        }
+    }
+}
