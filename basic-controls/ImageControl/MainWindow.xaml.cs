0743eeefa963d0f194ff7b09f6f9494e7bf7c432
haohiao <haohiao@qq.com>
2024-11-20 11:14:22 +0800

    add basic-controls

---
+++ basic-controls/ImageControl/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                Uri uri = new Uri(dialog.FileName);
+                ImageSource source = new BitmapImage(uri);
+
+                UniformImage.Source = source;
+                UniformToFillImage.Source = source;
+                FillImage.Source = source;
+                NoneImage.Source = source;
+            }
+        }
+    }
+}
