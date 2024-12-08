072b84486af9dc744ca83c16367dfa001bc69976
haohiao <haohiao@qq.com>
2024-12-08 17:48:55 +0800

    add data-binding parts

---
+++ data-binding/DataBindingViaCodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingViaCodeBehind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            Binding binding = new Binding();
+            binding.Source = ValueTextBox;
+            binding.Path = new PropertyPath("Text");
+
+            ValueTextBlock.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
---
+++ data-binding/UpdateSourceTriggerProperty/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UpdateSourceTriggerProperty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void ChangeTitleButton_Click(object sender, RoutedEventArgs e)
+        {
+            BindingExpression binding = TitleTextBox.GetBindingExpression(TextBox.TextProperty);
+            binding.UpdateSource();
+        }
+    }
+}
